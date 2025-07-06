cbae671c532883849c60a7716d31e93823d87fc9
manishiiitl1261 <manishrawat3703@gmail.com>
2025-07-06 15:27:50 +0530

    add some hight for review in mobile view

--- client/src/components/review/Card.jsx
+++ client/src/components/review/Card.jsx
@@ -86,7 +86,7 @@ const Card = ({ name, text, stars, brand, eventImage, image, userAvatar }) => {
                     </div>
 
                     {/* Event photo */}
-                    <div className="w-full sm:w-1/3 h-32 sm:h-40 rounded-lg overflow-hidden">
+                    <div className="w-full lg:w-1/3 h-72 rounded-lg overflow-hidden">
                         {displayEventImage ? (
                             <img
                                 src={displayEventImage}
